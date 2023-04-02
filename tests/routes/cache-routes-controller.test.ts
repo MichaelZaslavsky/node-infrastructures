924e9646cf559516939e8b0408e208b8dae9b4b3
MichaelZaslavsky <Michael.Zaslavsky@graduway.com>
2023-04-02 23:14:49 +0400

    Add cache routes tests

---
+++ tests/routes/cache-routes-controller.test.ts
@@ -0,0 +1,28 @@
+import request from "supertest";
+import { authenticatedAgent } from "../infrastructures/basicAuthentication";
+
+describe("Cache Routes Validation", () => {
+  describe("Create Key-Value Pair", () => {
+    it("should fail if key is missing", async () => {
+      const response = await authenticatedAgent()
+        .post("/cache")
+        .send({ value: "test-value" });
+
+      expect(response.status).toBe(400);
+      expect(response.body).toEqual({
+        errors: [
+          { field: "key", message: "Invalid value" },
+          { field: "key", message: "Key is required" },
+        ],
+      });
+    });
+
+    describe("Get Value by Key", () => {
+      it("should return a 404 error if the key does not exist", async () => {
+        const response = await authenticatedAgent().get("/cache");
+
+        expect(response.status).toBe(404);
+      });
+    });
+  });
+});
